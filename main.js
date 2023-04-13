039beee967c3db9ebe0b6a7e59eacf723d423ba0
KAyerdi <kevin.ayerdi@hotmail.com>
2023-04-13 02:28:45 -0300

    Proyecto-Rascatistas

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let typewriter = new Typewriter(app, {
  
 typewriter
   .pauseFor(2500)
-  .typeString('La Capital del Sol')
+  .typeString('La Capital del Mini Turismo')
   .pauseFor(200)
   .deleteChars(10)
   .start();
